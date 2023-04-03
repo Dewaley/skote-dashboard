586e143eadf8a542fb781790365457986d3961ff
Dewaley <adewaleademilua@gmail.com>
2023-04-03 06:00:26 +0100

    navbar desktop version

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,43 +5,33 @@ import {
   BiBell,
   BiChevronDown,
 } from "react-icons/bi";
-import {TbSettings} from "react-icons/tb"
+import { TbSettings } from "react-icons/tb";
 import pfp from "../assets/pfp.jpg";
 
 const Navbar = () => {
   return (
     <div className='Navbar'>
       <div className='left'>
-        <span className='hamburger'>
-          <IoIosMenu />
-        </span>
+        <IoIosMenu className='hamburger' />
         <form action=''>
-          <span className='search'>
-            <BiSearchAlt />
-          </span>
+          <BiSearchAlt className='search' />
           <input type='text' name='' id='' />
         </form>
       </div>
       <div className='right'>
-        <div className='customize'>
-          <span>
-            <BiCustomize />
-          </span>
-        </div>
+        <BiCustomize className='customize' />
         <div className='notification'>
           <span className='number'>3</span>
-          <span className='bell'>
-            <BiBell />
+          <span>
+            <BiBell className='bell' />
           </span>
         </div>
         <div className='user'>
           <img src={pfp} alt='User pfp' />
-          <span className='name'>admin</span>
-          <span className='arrow'>
-            <BiChevronDown />
-          </span>
+          <span className='name'>Maxwell</span>
+          <BiChevronDown />
         </div>
-        <div className="settings"><TbSettings/></div>
+        <TbSettings className='settings' />
       </div>
     </div>
   );
