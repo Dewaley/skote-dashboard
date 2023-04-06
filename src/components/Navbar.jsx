22cfe579c344b590ea2a052cd9d51fac2b9346a6
Dewaley <adewaleademilua@gmail.com>
2023-04-06 21:32:29 +0100

    initial loook for mobile design

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,8 +4,13 @@ import {
   BiCustomize,
   BiBell,
   BiChevronDown,
+  BiUserCheck,
 } from "react-icons/bi";
-import { TbSettings } from "react-icons/tb";
+import { MdOutlineSell } from "react-icons/md";
+import { BsWallet2, BsCalendar3, BsFileEarmarkText } from "react-icons/bs";
+import { TbSettings, TbArrowsExchange } from "react-icons/tb";
+import { FaBitcoin, FaOpencart, FaHandHoldingUsd } from "react-icons/fa";
+import { NavLink } from "react-router-dom";
 import pfp from "../assets/pfp.jpg";
 import { UseHamburger } from "../context/Hamburger";
 
@@ -42,6 +47,113 @@ const Navbar = () => {
         </div>
         <TbSettings className='settings' />
       </div>
+      <nav className={`deets ${hamburger && "active"}`}>
+        <h3>APPS</h3>
+        <ul>
+          <li>
+            <NavLink
+              to=''
+              end
+              className={({ isActive }) => (isActive ? "activeLink" : "link")}
+            >
+              <span className='icon'>
+                <BsWallet2 />
+              </span>
+              Wallet
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to='buy-sell'
+              className={({ isActive }) => (isActive ? "activeLink" : "link")}
+            >
+              <span className='icon'>
+                <MdOutlineSell />
+              </span>
+              Buy/Sell
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to='exchange'
+              className={({ isActive }) => (isActive ? "activeLink" : "link")}
+            >
+              <span className='icon'>
+                <TbArrowsExchange />
+              </span>
+              Exchange
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to='lending'
+              className={({ isActive }) => (isActive ? "activeLink" : "link")}
+            >
+              <span className='icon'>
+                <FaHandHoldingUsd />
+              </span>
+              Lending
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to='orders'
+              className={({ isActive }) => (isActive ? "activeLink" : "link")}
+            >
+              <span className='icon'>
+                <FaOpencart />
+              </span>
+              Orders
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to='kyc-application'
+              className={({ isActive }) => (isActive ? "activeLink" : "link")}
+            >
+              <span className='icon'>
+                <BiUserCheck />
+              </span>
+              KYC Application
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to='ico-landing'
+              className={({ isActive }) => (isActive ? "activeLink" : "link")}
+            >
+              <span className='icon'>
+                <BsCalendar3 />
+              </span>
+              Calendar
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to='ico-landing'
+              className={({ isActive }) => (isActive ? "activeLink" : "link")}
+            >
+              <span className='icon'>
+                <BsFileEarmarkText />
+              </span>
+              File Manager
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to='ico-landing'
+              className={({ isActive }) =>
+                isActive ? "activeLink link" : "link"
+              }
+            >
+              <span className='icon'>
+                <FaBitcoin />
+              </span>
+              ICO Landing
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 };
